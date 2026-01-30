eaf2b5fed9a1654a5f93bc58df62ca3a474211b5
Linear <63590436+Linear1024@users.noreply.github.com>
2026-01-30 19:54:31 +0800

    Add files via upload

---
+++ HAL/036-LCD/main.c
@@ -0,0 +1,216 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "adc.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdio.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+uint8_t ucSec;
+uint8_t ucKey, ucDir;
+uint8_t ucLed, ucSec1;
+uint8_t ucLcd[21];
+uint16_t usTlcd;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+void KEY_Proc(void);
+void LED_Proc(void);
+void LCD_Proc(void);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_ADC1_Init();
+  MX_ADC2_Init();
+  MX_TIM1_Init();
+  MX_TIM2_Init();
+  MX_TIM3_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+	LCD_Init();
+	HAL_Delay(100);
+	LCD_Clear(Black);
+	LCD_SetTextColor(White);
+	LCD_SetBackColor(Black);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+		KEY_Proc();
+		LED_Proc();
+		LCD_Proc();
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+
+/* USER CODE BEGIN 4 */
+void KEY_Proc(void)
+{
+	uint8_t ucKey_Val = 0;
+	ucKey_Val = KEY_Read();
+	if(ucKey_Val != ucKey)
+		ucKey = ucKey_Val;
+	else
+		ucKey_Val = 0;
+	
+	switch(ucKey_Val)
+	{
+		case 1:
+			ucDir ^= 1;
+			break;
+		case 2:
+			break;
+	}
+}
+
+void LED_Proc(void)
+{
+	if(ucSec1 == ucSec)
+		return;
+	ucSec1 = ucSec;
+	if(ucDir == 0)
+	{
+		ucLed <<= 1;
+		if(ucLed == 0)
+
+			ucLed = 1;
+	} else{
+		ucLed >>= 1;
+		if(ucLed == 0)
+			ucLed = 0x80;
+	}
+	LED_Disp(ucLed);
+}
+
+void LCD_Proc(void)
+{
+	if(usTlcd<500)
+		return;
+	usTlcd = 0;
+	sprintf((char*)ucLcd, "         %03u        ", ucSec);
+	LCD_DisplayStringLine(Line4, ucLcd);
+	LCD_DisplayChar(Line5, Column9, ucLcd[9]);
+	LCD_SetTextColor(Red);
+	LCD_DisplayChar(Line5, Column10, ucLcd[10]);
+	LCD_SetTextColor(White);
+	LCD_DisplayChar(Line5, Column11, ucLcd[11]);
+	LCD_SetTextColor(Red);
+	LCD_DisplayStringLine(Line6,ucLcd);
+	LCD_SetTextColor(White);
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
